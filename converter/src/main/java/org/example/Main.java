c669f389e715101660e253398e72611d0ec308ca
t.yantimirov <t.yantimirov@just-ai.com>
2022-11-26 15:34:12 +0300

    Add input processor

--- converter/src/main/java/org/example/Main.java
+++ converter/src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
+        InputProcessor inputProcessor = new InputProcessor();
+        if (inputProcessor.isCorrectConvertMode(args[0])) {
+            System.out.println("Please, enter correct convert mode. -bg or -gb");
+            return;
+        }
+        if (inputProcessor.isCorrectBinaryDigit(args[1])) {
+            System.out.println("Please, enter correct binary digit. You can input only 1 and 0");
+            return;
+        }
         int number = Integer.parseInt(args[1]);
         int result;
         switch (args[0]) {
